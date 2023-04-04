ef4f3ae3dae49fa522acfcf4c1b2bc7f38196cb7
Ayush Prajapati <ayush.prajapati@travenues.com>
2023-04-04 22:55:35 +0530

    initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FullScreen from './FullScreen'
+import MobileView from './MobileView'
+
+const Layout = () => {
+ 
+    console.log("hello")
+  return (
+    <div>
+      <div className='lg:block hidden '>
+        <FullScreen />
+      </div>
+      <div className='sm:block hidden xs:block lg:hidden'>
+        <MobileView />
+      </div>
+    </div>
+  )
+}
+
+export default Layout
